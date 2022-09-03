461247686641cb9e754edffe6e506168960161b3
OlekVano <olekvano@gmail.com>
2022-09-03 11:43:52 +0100

    Added home page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,39 @@
+import * as styles from '../styles/home.module.scss'
+
+import Header from '../components/Header'
+import Btn1 from '../components/Btn1'
+
 import Head from 'next/head'
+import Link from 'next/link'
 import Image from 'next/image'
+import daturaLogo from '../public/images/daturaLogo.png'
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>chat-app</title>
-        <meta name='description' content='Anonymous chat application' />
-        <link rel='icon' href='/favicon.ico' />
+        <title>Datura</title>
+        <meta name='description' content='The first trully anonymous messaging app' />
+        <link rel='icon' href='/images/daturaLogo.png' />
       </Head>
       <main>
-        <div>Hello</div>
+        <div className='roomsPage'>
+          <Header />
+          <div className='roomsPageContainer'>
+            <div className={`roomsPageMain ${styles.mainContainer}`}>
+              <div className={styles.halfContainer} >
+                <div className={styles.logoContainer}>
+                  <Image src={daturaLogo}></Image>
+                </div>
+              </div>
+              <div className={styles.halfContainer} >
+                <h1 className={styles.title} >Datura</h1>
+                <h2 className={styles.subtitle} >The first trully anonymous messaging app</h2>
+                <Btn1 text='Try it!' href='./rooms/create' />
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   )
