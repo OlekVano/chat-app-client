398e743246cb30c07b35467951a912d6b0fcbafc
OlekVano <olekvano@gmail.com>
2022-09-03 11:43:36 +0100

    Changed how Btn1 works. Can now work with both functions and links

--- components/Btn1.jsx
+++ components/Btn1.jsx
@@ -1,9 +1,10 @@
 import * as styles from './Btn1.module.scss'
 
-const Btn1 = ({ text, func={} }) => {
-  return (
-    <input type='button' value={text} onClick={func} className={styles.main}  />
-  )
+import Link from 'next/link'
+
+const Btn1 = ({ text, func={}, href='' }) => {
+  if (href === '') return <input type='button' value={text} onClick={func} className={styles.main} />
+  return <Link href={href}><input type='button' value={text} href='./rooms/create' className={styles.main} /></Link>
 }
 
 export default Btn1
